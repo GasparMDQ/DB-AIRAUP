f947c4dde7f8d08b2386264c617374e44f4968d5
Gaspar Zaragoza <gasparmdq@gmail.com>
2010-12-18 18:25:53 -0300

    Cambios menores
    Signed-off-by: Gaspar Zaragoza <gasparmdq@gmail.com>

--- includes/footer.php
+++ includes/footer.php
@@ -2,8 +2,9 @@
 </div>
   <div id="separadorB"></div>
   <div id="footer">
-	<p>&nbsp;&nbsp;&copy; 2010 Todos los derechos reservados A.I.R.A.U.P <br /> WebHosting cedido gentileza de Brokers Interactive, con presencia en <a href="http://www.mexico.com.mx" target="_blank">Mexico</a>, <a href="http://www.buenosaires.com.ar" target="_blank">Argentina</a> y <a href="http://www.santiagodechile.com" target="_blank">Chile</a>.</p>
-  <!-- end #footer --></div>
+	<p>&nbsp;&nbsp;&copy; 2010 Todos los derechos reservados<br />
+     WebHosting cedido gentileza de Brokers Interactive, con presencia en <a href="http://www.mexico.com.mx" target="_blank">Mexico</a>, <a href="http://www.buenosaires.com.ar" target="_blank">Argentina</a> y <a href="http://www.santiagodechile.com" target="_blank">Chile</a>.</p>
+<!-- end #footer --></div>
 <!-- end #container --></div>
 </body>
 </html>
--- index.php
+++ index.php
@@ -2,7 +2,11 @@
 include 'includes/header.php';
 ?>
 <p>Texto de bienvenida</p>
-&lt;listado de eventos proximos (5 eventos)&gt;
+<p>&lt;listado de eventos proximos (5 eventos)&gt;</p>
+<p>Ahora con nuevas funciones!<br>
+  <img src="images/animados/dancingjesus.gif" width="320" height="240">
+  <br>
+(a pedido de Marian)</p>
 <?php 
 include 'includes/footer.php';
 ?>
