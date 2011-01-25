258482f3ad691fdc37e98aa0a45ce4c8f836da49
Gaspar Zaragoza <gasparmdq@gmail.com>
2011-01-25 17:53:24 -0300

    Agregado de emails a los listados de socios por mesa
    Signed-off-by: Gaspar Zaragoza <gasparmdq@gmail.com>

--- admin/rrhh_eventos_mesas_listado_pdf.php
+++ admin/rrhh_eventos_mesas_listado_pdf.php
@@ -47,18 +47,15 @@ if ($evento!=0 AND $mesa!=0 AND $_POST['button']="Generar Listado") {
 <h2>Mesa: <?php echo $mesa_nombre; ?></h2>
 <h3>Coordinadores: <?php echo $coordina1." | ".$coordina2; ?></h3>
 <table width="90%" border="0">
-  <tr><th>Distrito</th><th>Nombre</th></tr>
+  <tr><th>Distrito</th><th>Nombre</th><th>E-Mail</th></tr>
 
 <?php
-	$sql="SELECT rtc_distritos.distrito, rtc_usr_personales.nombre, rtc_usr_personales.apellido FROM rtc_distritos, rtc_eventos_inscripciones, rtc_usr_personales, rtc_usr_institucional WHERE rtc_eventos_inscripciones.evento_id='$evento' AND rtc_eventos_inscripciones.mesa_id='$mesa' AND rtc_usr_personales.user_id=rtc_eventos_inscripciones.user_id AND rtc_usr_institucional.user_id=rtc_eventos_inscripciones.user_id AND rtc_distritos.id_distrito=rtc_usr_institucional.distrito ORDER BY rtc_distritos.distrito, rtc_usr_personales.apellido, rtc_usr_personales.nombre";
+	$sql="SELECT rtc_distritos.distrito, rtc_usr_personales.nombre, rtc_usr_personales.apellido, rtc_usr_login.email FROM rtc_distritos, rtc_eventos_inscripciones, rtc_usr_personales, rtc_usr_institucional, rtc_usr_login WHERE rtc_eventos_inscripciones.evento_id='$evento' AND rtc_eventos_inscripciones.mesa_id='$mesa' AND rtc_usr_personales.user_id=rtc_eventos_inscripciones.user_id AND rtc_usr_institucional.user_id=rtc_eventos_inscripciones.user_id AND rtc_distritos.id_distrito=rtc_usr_institucional.distrito AND rtc_usr_personales.user_id=rtc_usr_login.uid ORDER BY rtc_distritos.distrito, rtc_usr_personales.apellido, rtc_usr_personales.nombre";
 	$result=mysql_query($sql);
 	$cantidad_insc=mysql_num_rows($result);
 	echo "Cantidad de Inscriptos: ".$cantidad_insc."<br />";
-	while ($row=mysql_fetch_assoc($result)){
-		$distrito=$row['distrito'];
-		$nombre=$row['nombre']." ".$row['apellido'];
-?>
-  <tr><td><?php echo $distrito; ?></td><td><?php echo $nombre; ?></td></tr>
+	while ($row=mysql_fetch_assoc($result)){ ?>
+		<tr><td><?php echo $row['distrito']; ?></td><td><?php echo $row['nombre']." ".$row['apellido']; ?></td><td><?php echo $row['email']; ?></td></tr>
 <?php } ?>
 </table>
 </div>
