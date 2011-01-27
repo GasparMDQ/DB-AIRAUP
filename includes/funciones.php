ab04ba56b92bd74351aaf04ea62385358f9b8d11
Gaspar Zaragoza <gasparmdq@gmail.com>
2011-01-27 14:09:19 -0300

    Mejora del Home - Al estar logueado muestra los ultimos 5 encuentros asistidos y la asistencia a los mismos - Comienzo de archivo con funciones generales para reusar en la base de datos
    Signed-off-by: Gaspar Zaragoza <gasparmdq@gmail.com>

---
+++ includes/funciones.php
@@ -0,0 +1,33 @@
+<?
+//Archivo de funciones para la base de datos
+//Las funciones requieren que primero se cargue el header ya que necesitan que se establezcan las conexiones con la base de datos
+
+
+function user_porcen($uid,$ev,$me,$op) { // (User_id, Evento_id, Mesa_id, Opcion) Opcion=A: Asistencia REAL; Opcion=J: Asistencia JUSTIFICADA
+	$sql="SELECT * FROM rtc_eventos_mesa_modulos WHERE mesa_id='$me' AND fin!='0'";
+	$result_m = mysql_query($sql);
+	$modulos = mysql_num_rows($result_m); //CANTIDAD DE MODULOS FINALIZADOS EN LA MESA
+		
+//	echo "<p>UID:".$uid." T:".$modulos." A:".$asiste."</p>"; //VERIFICA POR USUARIO LOS MODULOS TOTALES Y LOS ASISTIDOS
+
+	if ($modulos==0) {
+		$modulos = 100; // SI NO HAY MODULOS EN LA MESA SE LO TOMA COMO QUE TIENE 100%
+		return $modulos;
+	} else {
+		if ($op=="A") {
+			$sql="SELECT DISTINCT rtc_eventos_mesa_modulos.id FROM rtc_eventos_asistencia, rtc_eventos_mesa_modulos WHERE rtc_eventos_asistencia.evento_id='$ev' AND rtc_eventos_asistencia.user_id='$uid' AND rtc_eventos_asistencia.mesa_id='$me' AND rtc_eventos_asistencia.modulo_id=rtc_eventos_mesa_modulos.id AND rtc_eventos_mesa_modulos.fin!='0' AND (rtc_eventos_asistencia.notas='' OR rtc_eventos_asistencia.notas='NULL') ";
+			$result = mysql_query($sql);
+			$asiste = mysql_num_rows($result); //CANTIDAD DE MODULOS ASISTIDOS POR EL USUARIO
+			return $asiste / $modulos * 100;
+		} else if ($op=="J"){
+			$sql="SELECT DISTINCT rtc_eventos_mesa_modulos.id FROM rtc_eventos_asistencia, rtc_eventos_mesa_modulos WHERE rtc_eventos_asistencia.evento_id='$ev' AND rtc_eventos_asistencia.user_id='$uid' AND rtc_eventos_asistencia.mesa_id='$me' AND rtc_eventos_asistencia.modulo_id=rtc_eventos_mesa_modulos.id AND rtc_eventos_mesa_modulos.fin!='0'";
+			$result = mysql_query($sql);
+			$asiste = mysql_num_rows($result); //CANTIDAD DE MODULOS ASISTIDOS POR EL USUARIO
+			return $asiste / $modulos * 100;
+		} else {
+			return 0;
+		}
+	}
+}
+
+?>
--- index.php
+++ index.php
@@ -1,8 +1,36 @@
 <?php 
 include 'includes/header.php';
+require_once 'includes/permisos.php';
+require_once 'includes/funciones.php';
+
 ?>
-<p>Bienvenidos!</p>
+<div>
+<h3>¡Bienvenido!</h3>
 <p>&lt;listado de eventos proximos (5 eventos)&gt;</p>
+</div>
+<?php // SI ESTA LOGUEADO
+if ($_SESSION['logged']) {
+?>
+<div>
+<h3>&Uacute;ltimos eventos asistidos</h3>
+
+<?php 
+	$user_id = $_SESSION['uid'];
+	$sql = "SELECT * FROM rtc_eventos, rtc_eventos_inscripciones WHERE rtc_eventos_inscripciones.user_id='$user_id' AND rtc_eventos.id=rtc_eventos_inscripciones.evento_id ORDER BY rtc_eventos.fecha_fin LIMIT 5";
+	$result=mysql_query($sql);
+	while ($row=mysql_fetch_assoc($result)) {
+		echo "<p>".$row['nombre']."<br />";
+		if ($row['fecha_fin']>date('Y-m-d')) {
+			echo "Asistencia: evento no finalizado <br />";
+		} else {
+			echo "Asistencia Real: ".number_format(user_porcen($user_id,$row['evento_id'],$row['mesa_id'],'A'),2)."<br />";
+			echo "Asistencia Justificada: ".number_format(user_porcen($user_id,$row['evento_id'],$row['mesa_id'],'J'),2)."</p>";
+		}
+	}
+?>
+
+</div>
+<?php } //FIN SI ESTA LOGUEADO?>
 <?php 
 include 'includes/footer.php';
 ?>
