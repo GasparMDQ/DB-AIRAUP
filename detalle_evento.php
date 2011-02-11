e063437f3c8747bc74d2694e47a21ec2e6d075d1
Gaspar Zaragoza <gasparmdq@gmail.com>
2011-02-11 13:05:42 -0300

    Correcion en la muestra de los detalles de los eventos - Habilitacion y deshabilitacion de botones - Control de inscripcion y etapas de la preinscripcion
    Signed-off-by: Gaspar Zaragoza <gasparmdq@gmail.com>

--- detalle_evento.php
+++ detalle_evento.php
@@ -16,11 +16,10 @@ $result=mysql_query($sql);
 $row=mysql_fetch_assoc($result);
 
 $baja="";
+$preinscripcion="";
 $fecha_fin=$row['fecha_fin'];
 $user_id=$_SESSION['uid'];
-if ($fecha_fin<date('Y-m-d')) {
-	$baja="disabled";
-}
+
 //PREINSCRIPCION
 if (isset($_POST['button']) AND $_POST['button']=="Preinscribirme" AND $nivel_usuario) {
 
@@ -42,6 +41,7 @@ if (isset($_POST['button']) AND $_POST['button']=="Preinscribirme" AND $nivel_us
 			$ok_tesoreria=$row['ok_tesoreria'];
 			$sql="INSERT INTO rtc_eventos_preinscripciones (evento_id, user_id, ok_club, ok_distrito, ok_tesoreria) VALUES ('$evento','$user_id','$ok_club','$ok_distrito','$ok_tesoreria')";
 			$result=mysql_query($sql);
+			$preinscripcion="disabled";
 		}
 	}
 }
@@ -55,6 +55,7 @@ if (isset($_POST['button']) AND $_POST['button']=="Dar de baja" AND $nivel_usuar
 	if (mysql_num_rows($result)) {
 		$sql="DELETE FROM rtc_eventos_preinscripciones WHERE evento_id='$evento' AND user_id='$user_id'";
 		$result=mysql_query($sql);
+		$preinscripcion="";
 	} else {
 		$sql="SELECT * FROM rtc_eventos_inscripciones WHERE user_id='$user_id' AND evento_id='$evento' LIMIT 1";
 		$result=mysql_query($sql);
@@ -105,17 +106,42 @@ if (isset($_POST['button']) AND $_POST['button']=="Dar de baja" AND $nivel_usuar
 
 		}
 	}
+	
+//Habilitacion de Botones
+$sql="SELECT * FROM rtc_eventos_preinscripciones WHERE user_id='$user_id' AND evento_id='$evento' LIMIT 1";
+$result=mysql_query($sql);
+if (mysql_num_rows($result)) {
+	$preinscripcion="disabled";
+} else {
+	$sql="SELECT * FROM rtc_eventos_inscripciones WHERE user_id='$user_id' AND evento_id='$evento' LIMIT 1";
+	$result=mysql_query($sql);
+	if (mysql_num_rows($result)) {
+		$preinscripcion="disabled";
+		$baja="";
+	} else {	
+		$baja="disabled";
+	}
+}
+
+if ($fecha_fin<date('Y-m-d')) {
+	$baja="disabled";
+}
+	
 ?>
 
 <div><h2><?php echo $row['nombre']; ?></h2></div>
+
+<?php if ($nivel_usuario) {?>
 <div class="texto_general">Estado de la inscripcion: <?php echo $status; ?></div>
+<?php } ?>
+
 <div>mail principal de contacto con el encuentro</div>
 <div>detalles del encuentro</div>
+<?php if ($nivel_usuario) {?>
 <div><form action="detalle_evento.php" method="post">
   <input name="evento" type="hidden" id="evento" value="<?php echo $evento; ?>" />
-  <input type="submit" name="button" value="Preinscribirme" />
+  <input <?php echo $preinscripcion; ?> type="submit" name="button" value="Preinscribirme" />
   <input <?php echo $baja; ?> type="submit" name="button" value="Dar de baja" />
 </form></div>
-<?php 
-include 'includes/footer.php';
-?>
+<?php } ?>
+<?php include 'includes/footer.php'; ?>
